66882a146a9775b03d0d079f8e8c0d533021c776
Abikananda <abikananda.2012@gmail.com>
2026-09-05 14:16:15 +0530

    env config

--- src/main/java/com/abikananda/ipo/IpoAnalysisApplication.java
+++ src/main/java/com/abikananda/ipo/IpoAnalysisApplication.java
@@ -1,5 +1,6 @@
 package com.abikananda.ipo;
 
+import com.abikananda.ipo.config.EnvContextInitializer;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cache.annotation.EnableCaching;
@@ -8,5 +9,9 @@ import org.springframework.scheduling.annotation.EnableScheduling;
 
 @SpringBootApplication @EnableScheduling @EnableAsync @EnableCaching
 public class IpoAnalysisApplication {
-  public static void main(String[] args) { SpringApplication.run(IpoAnalysisApplication.class, args); }
+  public static void main(String[] args) {
+    SpringApplication app = new SpringApplication(IpoAnalysisApplication.class);
+    app.addInitializers(new EnvContextInitializer());
+    app.run(args);
+  }
 }
